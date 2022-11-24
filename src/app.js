21fbbd8e47865fc39a5f81a62d439e1b715ea043
Martin Ferreira <martin.f.yic@gmail.com>
2022-11-24 16:54:52 -0300

    Start desafio NUESTRA PRIMERA BASE DE DATOS

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const { Server: IOServer } = require('socket.io');
 
 const path = require('path');
 const dotenv = require('dotenv');
-const v1ProdRouter = require('./v1/routes/productsRoutes');
 const v1WebSocket = require('./v1/routes/websocketRoutes');
 
 dotenv.config();
@@ -12,16 +11,15 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 const httpServer = new HttpServer(app);
-
-app.use(express.json());
 const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
-app.use(express.urlencoded({ extended: true }));
-app.use('/', v1WebSocket);
-app.use('/api/v1/productos', v1ProdRouter);
 
-app.set('views', './src/public/views');
-app.set('view engine', 'ejs');
+app
+	.use(express.json())
+	.use(express.static(publicPath))
+	.use(express.urlencoded({ extended: true }))
+	.use('/', v1WebSocket);
+
+app.set('views', './src/public/views').set('view engine', 'ejs');
 
 module.exports.io = new IOServer(httpServer);
 require('./sockets/socket');
